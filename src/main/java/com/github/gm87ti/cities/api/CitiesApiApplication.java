82c7d8688b17cbf845b5e40f077eaa3e86d21c69
Gabriel Moreti <gmoreti87@gmail.com>
2021-04-29 19:42:46 -0300

    Primeiro commit

---
+++ src/main/java/com/github/gm87ti/cities/api/CitiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.gm87ti.cities.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/github/gm87ti/cities/api/countries/repository/CountryRepository.java
@@ -0,0 +1,8 @@
+package com.github.gm87ti.cities.api.countries.repository;
+
+import com.github.gm87ti.cities.api.countries.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+}
